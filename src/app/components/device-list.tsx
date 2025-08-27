acaaabecbb1d65325af79bdb9bb24af2c85fa23e
richard <richard@ifocs.com>
2025-08-27 10:51:22 +0100

    remove next boilerplate page

--- src/app/components/device-list.tsx
+++ src/app/components/device-list.tsx
@@ -12,19 +12,26 @@ export const DeviceList = () => {
     setView(prevState => prevState === 'list' ? 'card' : 'list')
   }
 
+  const refresh = () => {
+    fetch('/api/v1/device').then((res) => {
+      return res.json()
+    }).then((json) => {
+      setData(json as DeviceSummary[])
+    })
+  }
+
   useEffect(() => {
     if (!data) {
-      fetch('/api/v1/device').then((res) => {
-        return res.json()
-      }).then((json) => {
-        setData(json as DeviceSummary[])
-      })
+      refresh()
     }
   })
 
   return <div>
     <div className="w-full sm:text-right mt-2">
-      <button className="border border-gray-500 rounded w-36 bg-gray-700 cursor-pointer" onClick={toggleView}>
+      <button className="border border-gray-500 rounded w-36 bg-gray-700 cursor-pointer mx-1" onClick={refresh}>
+        refresh
+      </button>
+      <button className="border border-gray-500 rounded w-36 bg-gray-700 cursor-pointer mx-1" onClick={toggleView}>
         go to {view === 'list' ? 'card' : 'list'} view
       </button>
     </div>
