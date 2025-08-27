429da0800405f534ed3726ffe5b8bfc2c05f4ebf
richard <richard@ifocs.com>
2025-08-27 09:00:31 +0100

    add table view

--- data/create.sql
+++ data/create.sql
@@ -4,7 +4,7 @@ insert into device_type (productTypeId, productName, description, dataSheetUrl, 
 values
     ('Ei1020', 'Environmental Sensor', 'Monitors temperature and humidity', 'https://www.aico.co.uk/homelink/wp-content/uploads/2025/03/Datasheet_Ei1020Ei1020_25-Mar-2025-1.pdf', 'https://www.aico.co.uk/homelink/wp-content/uploads/2025/03/Ei1020-Ei1025-Instructions-1.pdf'),
     ('Ei1025', 'Environmental Sensor', 'Monitors temperature, humidity and carbon dioxide', 'https://www.aico.co.uk/homelink/wp-content/uploads/2025/03/Datasheet_Ei1025Ei1025_25-Mar-2025.pdf', 'https://www.aico.co.uk/homelink/wp-content/uploads/2025/03/Ei1020-Ei1025-Instructions.pdf' ),
-    ('43521', 'Remote Thermostat', 'Controls underfloor heating', 'https://ambienteufh.co.uk/app/uploads/2023/09/Ambiente-LunaTouch-Datasheet-Black-and-White.pdf', 'https://ambienteufh.co.uk/app/uploads/2024/05/45368-Ambiente-LunaStat-Manual-0124.pdf');
+    ('Ei1234', 'Remote Thermostat', 'Controls underfloor heating', 'https://ambienteufh.co.uk/app/uploads/2023/09/Ambiente-LunaTouch-Datasheet-Black-and-White.pdf', 'https://ambienteufh.co.uk/app/uploads/2024/05/45368-Ambiente-LunaStat-Manual-0124.pdf');
 
 drop table if exists device;
 create table device ("id" integer PRIMARY KEY,"productUid" text,"productType" text,"status" text,"lastUpdated" integer,"deleteDate" integer);
--- src/app/components/device-list.tsx
+++ src/app/components/device-list.tsx
@@ -2,9 +2,15 @@
 import { DeviceSummary } from '@/app/api/v1/types'
 import { useEffect, useState } from 'react'
 import { DeviceDetails } from '@/app/components/device-details'
+import { DeviceTable } from '@/app/components/device-table'
 
 export const DeviceList = () => {
   const [data, setData] = useState<DeviceSummary[] | null>(null)
+  const [view, setView] = useState<'card' | 'list'>('list')
+
+  const toggleView = () => {
+    setView(prevState => prevState === 'list' ? 'card' : 'list')
+  }
 
   useEffect(() => {
     if (!data) {
@@ -16,7 +22,19 @@ export const DeviceList = () => {
     }
   })
 
-  return <ul className="my-2 flex flex-wrap lg:flex-row justify-center">
-    {data?.map((device) => <DeviceDetails key={device?.uid} deviceData={device} />)}
-  </ul>
+  return <div>
+    <div className="w-full sm:text-right mt-2">
+      <button className="border border-gray-500 rounded w-36 bg-gray-700 cursor-pointer" onClick={toggleView}>
+        go to {view === 'list' ? 'card' : 'list'} view
+      </button>
+    </div>
+    {
+      view === 'list' && <DeviceTable data={data} />
+    }
+    {
+      view === 'card' && <ul className="my-1 flex flex-wrap lg:flex-row justify-center space-y-0">
+        {data?.map((device) => <DeviceDetails key={device?.uid} deviceData={device} />)}
+      </ul>
+    }
+  </div>
 }
