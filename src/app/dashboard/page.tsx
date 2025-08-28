6f6a819af7b41ffe6760ffb393402cd773d074b6
richard <richard@ifocs.com>
2025-08-28 16:57:35 +0100

    remove server call

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -2,7 +2,6 @@ import { DeviceList } from '@/app/components/device-list'
 import { listDevices } from '@/app/api/v1/device/handlers/list-devices'
 
 export default async function Dashboard() {
-  const devices = await listDevices()
   return <main className="w-full text-center p-4">
     <h1 className="text-xl font-bold">Registered Homelink devices</h1>
     <DeviceList />
