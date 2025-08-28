ffa0ea20f46ee9c20870294bded5702192a68a4c
richard <richard@ifocs.com>
2025-08-28 17:01:29 +0100

    remove unused import

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,5 +1,4 @@
 import { DeviceList } from '@/app/components/device-list'
-import { listDevices } from '@/app/api/v1/device/handlers/list-devices'
 
 export default async function Dashboard() {
   return <main className="w-full text-center p-4">
